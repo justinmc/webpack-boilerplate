41bfda846c9a437e6041b7b1d270e5f9f4a5c9f5
Justin McCandless <justin.mccandless@teespring.com>
2016-04-29 19:56:18 -0700

    Eslint with airbnb eslintrc

--- public/js/main.js
+++ public/js/main.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-ReactDOM.render((
-    <div>
-      <h1>webpack-boilerplate</h1>
-    </div>
-  ),
+ReactDOM.render(
+  <div>
+    <h1>webpack-boilerplate</h1>
+  </div>,
   document.querySelector('.app')
 );
--- webpack.config.js
+++ webpack.config.js
@@ -2,23 +2,27 @@ var path = require('path');
 
 module.exports = {
   entry: {
-    app: ['./public/js/main.js']
+    app: ['./public/js/main.js'],
   },
   output: {
     path: path.resolve(__dirname, 'dist'),
     publicPath: '/js/',
-    filename: 'bundle.js'
+    filename: 'bundle.js',
   },
   devServer: {
     port: '4000',
-    hot: true
+    hot: true,
   },
   module: {
     loaders: [
       {
         test: /\.jsx?$/,
         exclude: /(node_modules|bower_components)/,
-        loader: 'babel'
+        loader: 'babel',
+      }, {
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'eslint-loader',
       }
     ]
   }
